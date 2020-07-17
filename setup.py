b0fea2df4de671b557704f881c9e95339642d703
dhvandenakker <d.h.vandenakker@live.nl>
2020-07-17 13:28:38 +0200

    version based on git tag

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
+import git
 from setuptools import setup, find_packages
+
+def get_version():
+    g = git.cmd.Git()
+    version = g.describe()
+    return version
+
 
 if __name__ == "__main__":
     setup(
+        version=get_version(),
         packages=find_packages(exclude=["*tests*"])
     )
